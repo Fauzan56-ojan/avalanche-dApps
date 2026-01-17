f8b2ba733f080eadc2b2e74202fa606a3783630a
Fauzan <kholidfauzan56@gmail.com>
2026-01-17 14:35:23 +0700

    create BE nestjs

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+   const config = new DocumentBuilder()
+    .setTitle('Simple Storage dApp API')
+    .setDescription(
+    'The Simple Storage description<br/>' +
+    'Fauzan Kholid (221011402763)'
+    )
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('dokumentation', app, documentFactory);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
